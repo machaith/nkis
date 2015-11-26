6fe8933d07921604824b700cb28a1e633ab1ff40
Haitham Mohamed Saad <haitham.mohamed@hotmail.com>
2015-11-26 19:21:31 +0300

    fix accidentally deleted code

--- nkis.sh
+++ nkis.sh
@@ -28,4 +28,24 @@ cd /root
 elif [ "$ver" == "$v2" ];then
 echo "vmware"
 else
+echo "No virtualization detected"
+echo "installing gedit"
+apt-get install gedit
+echo "Configuring postgresql database to autostart on boot"
+update-rc.d postgresql enable
+echo installing additional tools"
+echo "*************************************************************"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "installing masscan"
+echo "This is the fastest Internet port scanner. It can scan the entire Internet in under six minutes"
+echo "Source is @https://github.com/robertdavidgraham/masscan
+apt-get install git gcc make libpcap-dev
+git clone https://github.com/robertdavidgraham/masscan.git /opt/masscan
+cd /opt/masscan
+make
+make install
+echo "Do not forget to change the hostname using "gedit /etc/hostname""
                                                                                                                                     1,1           Top
