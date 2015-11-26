22948fb0f52207719505593b2e86bdfe20535e49
machaith <root@localhost.localdomain>
2015-11-26 11:32:39 +0300

    Simple

--- nkis.sh
+++ nkis.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+echo "*************************************************************"
+echo "Created by machaith"
+echo "https://github.com/machaith"
+echo "updating your machine"
+echo "*************************************************************"
 apt-get update && apt-get upgrade
+echo "Updates were installed, do not forget to run "apt-get dist-upgrade" and reboot"
+echo ""
+echo ""
+echo ""
+echo "preparing the github folder to download more tools"
+cd ~
+mkdir github
+cd github/
+apt-get install gedit
