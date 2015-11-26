4d9ce5097402f97ca19dfdb38f95ac707eba883d
Haitham Mohamed Saad <haitham.mohamed@hotmail.com>
2015-11-26 17:51:54 +0300

    virtualbox additions working

--- nkis.sh
+++ nkis.sh
@@ -14,34 +14,18 @@ echo ""
 echo ""
 echo ""
 echo "Checking if installed as a virtual and if Vmware_tools or VirtualBox-additions is needed"
-v1 = "VirtualBox"
-v2 = "VMware Virtual Platform"
-ver = `dmidecode -s system-product-name`
-if [ "$ver" == "$v1"]
+v1="VirtualBox";
+v2="VMware Virtual Platform";
+ver=`dmidecode -s system-product-name`;
+echo "$ver"
+if [ "$ver" == "$v1" ];then
 apt-get update && apt-get install -y linux-headers-$(uname -r)
 read -p "Choose the install VirtualBox/Vmware tools  from the menu then press enter"
 cp /media/cdrom/VBoxLinuxAdditions.run /root/
 chmod 755 /root/VBoxLinuxAdditions.run
 cd /root
 ./VBoxLinuxAdditions.run
-cd
-echo "installing gedit"
-apt-get install gedit
-echo "Configuring postgresql database to autostart on boot"
-update-rc.d postgresql enable
-echo installing additional tools"
-echo "*************************************************************"
-echo ""
-echo ""
-echo ""
-echo ""
-echo "installing masscan"
-echo "This is the fastest Internet port scanner. It can scan the entire Internet in under six minutes"
-echo "Source is @https://github.com/robertdavidgraham/masscan
-apt-get install git gcc make libpcap-dev
-git clone https://github.com/robertdavidgraham/masscan.git /opt/masscan
-cd /opt/masscan
-make
-make install
-echo "Do not forget to change the hostname using "gedit /etc/hostname""
-
+elif [ "$ver" == "$v2" ];then
+echo "vmware"
+else
+                                                                                                                                    1,1           Top
