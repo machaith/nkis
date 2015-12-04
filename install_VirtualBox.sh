9324f9a1d8917ac7692f9a971e3cda5fc0dcf9a8
Haitham Mohamed Saad <haitham.mohamed@hotmail.com>
2015-12-04 19:21:12 +0300

    fix_minor_issue

--- install_VirtualBox.sh
+++ install_VirtualBox.sh
@@ -4,8 +4,7 @@ apt-get update && apt-get install -y linux-headers-$(uname -r)
 read -p "Choose the install VirtualBox/Vmware tools  from the menu then press enter"
 cp /media/cdrom/VBoxLinuxAdditions.run /root/
 chmod 755 /root/VBoxLinuxAdditions.run
-cd /root
-./VBoxLinuxAdditions.run
+/root/VBoxLinuxAdditions.run
 echo VBox guest Additions" installed"
 read -p "press Enter to start installing the tools"
 ./install_tools.sh
